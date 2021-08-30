83cb4acddf5e9dc45aca19cc6542d91ef829ece0
prithvi-jpg <mv.prithvi@gmail.com>
2021-08-31 00:05:46 +0530

    Header part

---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+//import header css
+import './Header.css';
+import { TextField, ThemeProvider } from '@material-ui/core';
+import { createTheme} from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import categories from '../../data/category';
+
+
+const Header = ({ setCategory, category, word, setWord}) => {
+    const darkTheme = createTheme({
+        palette: {
+            primary: {  
+                main: '#fff',
+            },
+          type: 'dark',
+        },
+      });
+    
+      const handleChange = (language) => {
+        setCategory(language);
+        setWord('');
+      };
+
+    return (
+        <div className="header">
+            <span className="title">{word?word: " Word Hunt "}</span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                <TextField 
+                    className="search"
+                    label="Search a word"
+                    id="standard-basic"
+                    value = {word}
+                    onChange = {(event) => setWord(event.target.value)}     
+                />
+                <TextField
+          className="select"
+          select
+          label="Language" 
+          value= {category}
+          onChange={(e) => handleChange(e.target.value)}
+        >   
+            {categories.map((option) => (
+                    <MenuItem key={option.label} value={option.label}>{option.value}</MenuItem>
+                ))}
+        </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
